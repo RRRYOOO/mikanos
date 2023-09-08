eb66a32f2d0140452c7224649078446f2bd2968d
RRRYOOO <damdagqtjp@gmail.com>
2023-09-08 11:38:29 +0900

    create osbook_04c

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -8,43 +8,83 @@ struct PixelColor {
     uint8_t r, g, b;
 };
 
-/* 1ピクセルを指定した色で塗る関数（戻り値 0：成功、0以外：失敗） */
-int WritePixel(const FrameBufferConfig& config,
-                int x, int y, const PixelColor& color) {
-    const int pixel_position = config.pixels_per_scan_line * y + x; // 指定されたピクセルが先頭から何番目の位置にあるかを計算
-    uint8_t* p = &config.frame_buffer[4 * pixel_position];      // 指定されたピクセルのフレームのアドレスをPに格納
-    /* ピクセルのデータ形式がRGBの場合 */
-    if (config.pixel_format == kPixelRGBResv8VitPerColor) {
-        p[0] = color.r;     // ピクセルのフレームに赤の設定値を書き込み
-        p[1] = color.g;     // ピクセルのフレームに緑の設定値を書き込み
-        p[2] = color.b;     // ピクセルのフレームに青の設定値を書き込み
-    }
-    /* ピクセルのデータ形式がBGRの場合 */
-    if (config.pixel_format == kPixelBGRResv8VitPerColor) {
-        p[0] = color.b;     // ピクセルのフレームに青の設定値を書き込み
-        p[1] = color.g;     // ピクセルのフレームに緑の設定値を書き込み
-        p[2] = color.r;     // ピクセルのフレームに赤の設定値を書き込み
-    }
-    /* それ以外の場合 */
-    else {
-        return -1;  // 失敗
-    }
-    return 0;   // 成功
+/* PixelWriterの親クラス */
+class PixelWriter {
+ public:
+  PixelWriter(const FrameBufferConfig& config) : config_{config} {      // コンストラクタ
+  }
+  virtual ~PixelWriter() = default;     // デストラクタ
+  virtual void Write(int x, int y, const PixelColor& c) = 0;       // ピクセル描画関数
+
+ protected:
+  uint8_t* PixelAt(int x, int y) {
+    return config_.frame_buffer + 4 * (config_.pixels_per_scan_line * y + x);        // 指定されたピクセルのフレームのアドレスを返す
+  }
+
+ private:
+  const FrameBufferConfig& config_;     // フレームバッファ情報
+};
+
+/* PixelWriterの子クラス（ピクセルのデータ形式がRGBの場合） */
+class RGBResv8BitPerColorPixelWriter : public PixelWriter {
+ public:
+  using PixelWriter::PixelWriter;    // 親クラスのコンストラクタをそのまま使用
+
+  virtual void Write(int x, int y, const PixelColor& c) override {      // 親クラスのピクセル描画関数をオーバーライド
+    auto p = PixelAt(x, y);     // 指定されたピクセルのフレームアドレスを取得
+    p[0] = c.r;
+    p[1] = c.g;
+    p[2] = c.b;
+  }
+};
+
+/* PixelWriterの子クラス（ピクセルのデータ形式がBGRの場合） */
+class BGRResv8BitPerColorPixelWriter : public PixelWriter {
+ public:
+  using PixelWriter::PixelWriter;    // 親クラスのコンストラクタをそのまま使用
+
+  virtual void Write(int x, int y, const PixelColor& c) override {      // 親クラスのピクセル描画関数をオーバーライド
+    auto p = PixelAt(x, y);     // 指定されたピクセルのフレームアドレスを取得
+    p[0] = c.b;
+    p[1] = c.g;
+    p[2] = c.r;
+  }
+};
+
+/* 配置new用の関数 */
+void* operator new(size_t size, void* buf) {
+    return buf;     // 引数で受け取った領域のアドレスをそのままかえるだけ
+}
+/* delete用の関数（使用していないけど、デストラクタの中で要求されるため） */
+void operator delete(void* obj) noexcept {
 }
 
-extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config)
-{
+/* クラスのインスタンス用に領域を確保する */
+char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
+/* クラスのインスタンスのポインタの格納用（この変数を用いて操作を行う） */
+PixelWriter* pixel_writer;
+
+extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
+    switch (frame_buffer_config.pixel_format) {     // ピクセルのデータ形式によってインスタンス化するクラスを分ける
+      case kPixelRGBResv8BitPerColor:       // ピクセルのデータ形式がRGBの場合
+        pixel_writer = new(pixel_writer_buf)        // クラスのインスタンス用に確保した領域を割り当てる
+          RGBResv8BitPerColorPixelWriter{frame_buffer_config};      // RGB用の子クラスをインスタンス化する
+        break;
+      case kPixelBGRResv8BitPerColor:       // ピクセルのデータ形式がBGRの場合
+        pixel_writer = new(pixel_writer_buf)        // クラスのインスタンス用に確保した領域を割り当てる
+          BGRResv8BitPerColorPixelWriter{frame_buffer_config};      // BGR用の子クラスをインスタンス化する
+        break;
+    }
     /* 一旦、画面を白で塗りつぶす */
     for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {  // 横（0～最後まで）
         for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y) { // 縦（0～最後まで）
-            WritePixel(frame_buffer_config, x, y, {255, 255, 255}); //　該当ピクセルを白で塗りつぶす
+            pixel_writer->Write(x, y, {255, 255, 255}); //　該当ピクセルを白で塗りつぶす
         }
     }
-
     /* 指定した位置を緑に塗る*/
     for (int x = 0; x < 200; ++x) {     // 横（0～199まで）
         for (int y = 0; y < 100; ++y) {     // 横（0～99まで）
-            WritePixel(frame_buffer_config, 100 + x, 100 + y, {0, 255, 0}); // 開始位置から横に100+x、縦に100+yの位置を緑に塗る
+            pixel_writer->Write(x, y, {0, 255, 0}); // 開始位置から横にx、縦にyの位置を緑に塗る
         }
     }
     while(1) __asm__("hlt");
